7a6f15ac47150ed2c5ac87e800168566a196ca0d
imachraoui <ismail.machraoui@gmail.com>
2016-04-09 23:01:50 +0200

    Update requests.sql

--- requests.sql
+++ requests.sql
@@ -24,3 +24,18 @@ join ingredient ing
 on ing.id_ingredient = c.id_ingredient
 group by ing.nom, cafe.code_postal)
 group by substr(code_postal,0,3)
+
+-- p)
+select i.nom, SUM(ci.nombre) as quantite
+from commande
+join cafe 
+on cafe.id_cafe = commande.id_cafe
+join commande_item ci
+on commande.id_commande = ci.id_commande
+join client_vip cv
+on cv.id_client = commande.id_vip
+join item i
+on i.id_item= ci.id_item
+where cafe.pays='FRANCE' and (2016 - strftime('%Y',cv.date_naissance)) <=40 and commande.id_vip IS NOT NULL
+group by ci.id_item
+order by quantite DESC
